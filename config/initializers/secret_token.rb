75b087adff2dde216d8d51b4e011dccb49bfca30
awaxman11 <awaxman11@gmail.com>
2013-06-13 15:35:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlockLandingLaunch::Application.config.secret_token = 'e9c62e8e62dfa694462c26fc7f6b49295517de615db765165331b91453446023a0b4694b1d91f77b964c320c3dbc1cc2bc69fc6efb5ed5bfc390584e2fef1bbb'
